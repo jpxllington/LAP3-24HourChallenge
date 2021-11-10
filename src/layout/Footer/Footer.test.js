cbc03211f7ea9f533440628dd9bd477b991d2324
“yourGithubUsername” <your@emailadd.com>
2021-11-10 18:01:21 +0000

    add UserProfile tests

--- src/layout/Footer/Footer.test.js
+++ src/layout/Footer/Footer.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 
 describe('Footer', () => {
 
-    it('renders footer', () => {
+    test('renders footer', () => {
         render(<Footer />)
         const footer = screen.getByRole('footer');
         expect(footer.textContent).toBe('James & Zerh');
--- src/layout/Header/Header.test.js
+++ src/layout/Header/Header.test.js
@@ -1,12 +1,12 @@
-import { screen } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import Header from "./index";
 
 describe("Header", () => {
 
-    it("renders a header", () => {
+    test("renders a header", () => {
         renderWithReduxAndRouter(<Header />);
         expect(screen.getByRole("heading").textContent).toBe(
-            "GitHub Repo Tracker");
+        "GitHub Repo Tracker");
   });
 
 });
