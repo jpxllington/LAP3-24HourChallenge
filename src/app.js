09da7ee1cd022e4f70ade5daf72b94e2b6e2a685
“yourGithubUsername” <your@emailadd.com>
2021-11-10 11:25:51 +0000

    change react-router-dom version

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Header, Footer } from './layout/Header/index'
+import { Switch, Route } from "react-router-dom";
 import './style.css'
 
 function App () {
     return (
         <>
             <Header />
-            <h1>hello</h1>
-
+            <Switch>
+                <Route exact path="/">
+                    <Pages.Welcome />
+                </Route>
+                <Route path="/user" >
+                        <Pages.About />
+                </Route>
+            </Switch>
         </>
     )
 }
