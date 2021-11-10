6d8a60fbb1165bae44e7cee7395ad87cf57a7efa
jpxllington <jjdp99@gmail.com>
2021-11-10 12:05:11 +0000

    install storybook, stub out RepoCard

--- src/components/RepoCard/index.jsx
+++ src/components/RepoCard/index.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
-export const RepoCard = () => {
+export const RepoCard = (repo) => {
+
+    const[ expanded, setExpanded] = useState(false)
+
+    const expand = () => {
+        setExpanded(!expanded)
+    }
 
     return(
-        <p>something should be here</p>
+        <>  
+            {!expanded && (
+                <div className="RepoCard card-sm">
+                    <h1>{repo.name}</h1>
+                    <p>Owner: {repo.owner.login}</p>
+                    <div className="buttonHolder">
+                        <a className="gitButton" href={repo.html_url} target="_blank">GitHubLogoHere</a>
+                        <button className="moreDetails" onClick={expand}>View Details</button>
+                    </div>
+                </div>)}
+            {expanded && (
+                <div className="RepoCard card-lg">
+                    <h1>{repo.name}</h1>
+                    <p>Owner: {repo.owner.login}</p>
+                    <p>Number of forks: {repo.forks_count}</p>
+                    <p>Number of stargazers {repo.stargazers_count}</p>
+                    <div className="buttonHolder">
+                        <a className="gitButton" href={repo.html_url} target="_blank">GitHubLogoHere</a>
+                        <button className="lessDetails" onClick={expand}>X</button>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
